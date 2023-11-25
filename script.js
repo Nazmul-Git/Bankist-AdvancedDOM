d61ea85fb0aa581efd02108a1c93c7703f3392e9
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-25 14:42:14 +0600

    Implement scrolling effect.

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+console.log(btnsOpenModal);
+/**
+NodeList(2) [ a.nav__link.nav__link--btn.btn--show-modal, button.btn.btn--show-modal ]
+ */
 
 const openModal = function (e) {
   e.preventDefault();
@@ -28,4 +32,14 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const btnScrollTo=document.querySelector('.btn--scroll-to');
+const section1=document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', (e)=>{
+  // const scrolling=section1.getBoundingClientRect();
+  // console.log(scrolling);
+  // console.log(e.target.getBoundingClientRect());
+
+  section1.scrollIntoView({behavior: 'smooth'});
 });
