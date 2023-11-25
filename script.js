3a443d8baa0506570f6b661454f61ac9535ea967
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-26 00:07:04 +0600

    Implement nav links container color, change background color by randomize number..

--- script.js
+++ script.js
@@ -54,10 +54,17 @@ const randomInt=(min, max)=>Math.floor(Math.random()*(max-min+1)+min);
 // random color function
 const randomColor=()=>`rgb(${randomInt(0,255)}, ${randomInt(0,255)}, ${randomInt(0,255)})`;
 
+// link
 document.querySelector('.nav__link').addEventListener('click', function(e){
   this.style.backgroundColor=randomColor();
   console.log('link', e.target);
-})
+});
+
+// links container
+document.querySelector('.nav__links').addEventListener('click', function(e){
+  this.style.backgroundColor=randomColor();
+  console.log('links container', e.target);
+});
 
 
 
